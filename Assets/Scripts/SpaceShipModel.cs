1372f25cd16e0192998b61976b325c214560e070
Ben Shapiro <bshap93@gmail.com>
2024-08-27 18:30:17 -0600

    First commit

---
+++ Assets/Scripts/SpaceShipModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipModel 
+{
+    [SerializeField]  float speed;
+    [SerializeField]  float health;
+    [SerializeField] Vector3 position;
+
+}
